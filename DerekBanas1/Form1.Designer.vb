9826e679c1c9e5e2200d8a959422af512cd39a99
CosminManu <cosmin.manu2000@gmail.com>
2024-10-25 20:09:32 +0300

    first commit derek

--- DerekBanas1/Form1.Designer.vb
+++ DerekBanas1/Form1.Designer.vb
@@ -22,10 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnMessage = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(56, 58)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(55, 20)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(104, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 16)
+        Me.Label2.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(117, 56)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(212, 22)
+        Me.txtName.TabIndex = 2
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(59, 109)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(195, 34)
+        Me.btnOk.TabIndex = 3
+        Me.btnOk.Text = "&OK"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(501, 109)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(195, 34)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnMessage
+        '
+        Me.btnMessage.Location = New System.Drawing.Point(281, 109)
+        Me.btnMessage.Name = "btnMessage"
+        Me.btnMessage.Size = New System.Drawing.Size(195, 34)
+        Me.btnMessage.TabIndex = 5
+        Me.btnMessage.Text = "&Message"
+        Me.btnMessage.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnMessage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblName As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnMessage As Button
 End Class
